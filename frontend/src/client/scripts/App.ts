3be3375828dfaf5af4e394cb0a3a35b4ef30f2e8
Florian Gudat <florian.gudat@guices.com>
2019-12-10 23:21:23 +0100

    API adjustments

--- frontend/src/client/scripts/App.ts
+++ frontend/src/client/scripts/App.ts
@@ -1,47 +1,22 @@
 import {
     MessagingEndpoints,
-    EndpointProperties,
-    Message,
     MessagingChannel,
     MessagingSystem,
     LoggerSingleton
 } from "@scsa/messaging";
-import { cfg } from "../../config";
 
 export class App implements MessagingEndpoints {
-    properties: EndpointProperties = cfg.CURRENT;
     channel: MessagingChannel;
 
     constructor(messagingSystem: MessagingSystem) {
-        this.channel = messagingSystem.channel;
-        this.channel.subscribe(this, cfg.APPLICATIONS.Sales.options.text);
-        this.channel.subscribe(this, cfg.APPLICATIONS.Catalogue.options.text);
-        this.channel.subscribe(this, cfg.APPLICATIONS.Checkout.options.text);
+        this.channel = messagingSystem.options.channel;
+        this.channel.subscribe(this);
     }
 
     handleEndpoint(data: any) {
         try {
             const instance = LoggerSingleton.getInstance();
             instance.write(data);
-        } catch (error) {
-            console.warn(error);
-        }
-    }
-
-    publish() {
-        try {
-            this.channel.publish(
-              new Message({ hello: "Account, are you there?" }),
-              cfg.APPLICATIONS.Sales.options.text
-            );
-            this.channel.publish(
-              new Message({ hello: "Catalogue, are you there?" }),
-              cfg.APPLICATIONS.Catalogue.options.text
-            );
-            this.channel.publish(
-              new Message({ hello: "Checkout, are you there?" }),
-              cfg.APPLICATIONS.Checkout.options.text
-            );
         } catch (error) {
             console.warn(error);
         }

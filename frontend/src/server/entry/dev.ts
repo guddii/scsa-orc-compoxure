073b5d4c680fcee28e2af0bf26af06d04171d514
Florian Gudat <florian.gudat@guices.com>
2019-12-11 23:26:59 +0100

    Add ESI

--- frontend/src/server/entry/dev.ts
+++ frontend/src/server/entry/dev.ts
@@ -3,7 +3,7 @@ import { Blueprint } from "@scsa/styling/src/dev";
 import { cfg } from "../../config";
 
 // Initialize default application
-const app = new Blueprint(cfg).mounts();
+const app = new Blueprint(cfg).esi().mounts();
 
 // Assign application to listener
 app.listen(cfg.PORT);

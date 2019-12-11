d4cc6f7e54c68f4e062f77fe9a35d43342d58eb5
Florian Gudat <florian.gudat@guices.com>
2019-12-12 00:04:35 +0100

    Dependency fix

--- frontend/src/client/scripts/index.ts
+++ frontend/src/client/scripts/index.ts
@@ -1,7 +1,7 @@
 import { MessagingSystemFactory, SecurityChecks } from "@scsa/messaging";
 
 import { cfg } from "../../config";
-import { App } from "../../../../../iframe/frontend/src/client/scripts/App";
+import { App } from "./App";
 
 const securityChecks = new SecurityChecks(cfg.endpoints());
 const messaging = MessagingSystemFactory.create({ security: securityChecks });
